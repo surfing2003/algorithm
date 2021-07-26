be192a3c825d5f3f2ecf4e2f58e3147de0247c83
surfing2003 <surfing2003@naver.com>
2021-07-26 23:44:18 +0900

    algo_study

--- pr05.py
+++ pr05.py
@@ -688,50 +688,50 @@
 # print(*answer)
 
 ##  2981 풀이 수정 + 코드 리뷰
-from math import gcd, sqrt
+# from math import gcd, sqrt
 
-N = int(input())
-arr = sorted(list(int(input()) for _ in range(N)))
-interval = []
-answer = []
+# N = int(input())
+# arr = sorted(list(int(input()) for _ in range(N)))
+# interval = []
+# answer = []
 
-for i in range(1,N):
-    interval.append(arr[i] - arr[i-1])
+# for i in range(1,N):
+#     interval.append(arr[i] - arr[i-1])
 
-prev = interval[0]
-for i in range(1,len(interval)):
-    prev = gcd(prev, interval[i])
+# prev = interval[0]
+# for i in range(1,len(interval)):
+#     prev = gcd(prev, interval[i])
 
-for i in range(2, int(sqrt(prev)) + 1):
-    if prev % i == 0:
-        answer.append(i)
-        answer.append(prev//i)
-answer.append(prev)
-ans = sorted(list(set(answer))) 
-print(*ans)
+# for i in range(2, int(sqrt(prev)) + 1):
+#     if prev % i == 0:
+#         answer.append(i)
+#         answer.append(prev//i)
+# answer.append(prev)
+# ans = sorted(list(set(answer))) 
+# print(*ans)
 
-##  2981 참고
-from math import gcd
-from math import sqrt
+# ##  2981 참고
+# from math import gcd
+# from math import sqrt
 
-n = int(input())
-ns = list(int(input()) for _ in range(n))
-ns.sort()
-interval = list()
-ans = list()
+# n = int(input())
+# ns = list(int(input()) for _ in range(n))
+# ns.sort()
+# interval = list()
+# ans = list()
 
-for i in range(1, n):
-    interval.append(ns[i] - ns[i - 1])
+# for i in range(1, n):
+#     interval.append(ns[i] - ns[i - 1])
 
-prev = interval[0]
-for i in range(1, len(interval)):
-    prev = gcd(prev, interval[i])
+# prev = interval[0]
+# for i in range(1, len(interval)):
+#     prev = gcd(prev, interval[i])
 
-for i in range(2, int(sqrt(prev)) + 1): #제곱근까지만 탐색
-    if prev % i == 0:
-        ans.append(i)
-        ans.append(prev//i)
-ans.append(prev)
-ans = list(set(ans)) #중복이 있을수 있으니 제거
-ans.sort()
-print(*ans)
+# for i in range(2, int(sqrt(prev)) + 1): #제곱근까지만 탐색
+#     if prev % i == 0:
+#         ans.append(i)
+#         ans.append(prev//i)
+# ans.append(prev)
+# ans = list(set(ans)) #중복이 있을수 있으니 제거
+# ans.sort()
+# print(*ans)
